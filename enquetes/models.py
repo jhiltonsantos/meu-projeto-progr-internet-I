612d0dee6451ebaac4db553cd9448eefd867dc09
Hilton Santos <jhilton930@gmail.com>
2020-08-29 22:35:36 -0300

    Update Atividade 3

--- enquetes/models.py
+++ enquetes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Enquete(object):
+    def __init__(self, question1='', question2='', question3='', data_publicacao='',):
+        self.question1 = question1
+        self.question2 = question2
+        self.question3 = question3
+        self.data_publicacao = data_publicacao
--- enquetes/views.py
+++ enquetes/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from enquetes.models import Enquete
 
-# Create your views here.
+
 def bemvindo(request):
     return render(request, 'bemvindo.html')
+
+
+def enquete(request, enquete_id):
+    enquete = Enquete()
+
+    if enquete_id == 1:
+        enquete = Enquete("Qual o seu nome?",
+                          "Em qual cidade voce nasceu?",
+                          "Qual a sua faculdade que voce cursa?",
+                          "2020-08-25")
+
+    if enquete_id == 2:
+        enquete = Enquete("Quantas horas por semana voce usa redes sociais?",
+                          "Quantas horas por semana voce passa jogando?",
+                          "Quantas horas por semana voce passa estudando?",
+                          "2020-08-26")
+
+    if enquete_id == 3:
+        enquete = Enquete("Qual linguagem de programacao voce esta estudando?",
+                          "Quando voce iniciou os estudos nessa linguagem?",
+                          "Por qual meio voce esta estudando? (livro, youtube, cursos, etc)",
+                          "2020-08-27")
+    return render(request, 'enquete.html', {"enquete": enquete})
