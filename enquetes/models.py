380fe2912cb44fb337727287df2e81928563e87f
Hilton Santos <jhilton930@gmail.com>
2020-09-05 18:09:39 -0300

    Update Atividade 4

--- enquetes/models.py
+++ enquetes/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-class Enquete(object):
-    def __init__(self, question1='', question2='', question3='', data_publicacao='',):
-        self.question1 = question1
-        self.question2 = question2
-        self.question3 = question3
-        self.data_publicacao = data_publicacao
+class Enquete(models.Model):
+    texto = models.CharField(max_length=100)
+    data_publicacao = models.DateField()
+    
+    
--- enquetes/views.py
+++ enquetes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from enquetes.models import Enquete
 
@@ -8,23 +8,6 @@ def bemvindo(request):
 
 
 def enquete(request, enquete_id):
-    enquete = Enquete()
+    enquete = {'enquete': get_object_or_404(Enquete, id=enquete_id)}
 
-    if enquete_id == 1:
-        enquete = Enquete("Qual o seu nome?",
-                          "Em qual cidade voce nasceu?",
-                          "Qual a sua faculdade que voce cursa?",
-                          "2020-08-25")
-
-    if enquete_id == 2:
-        enquete = Enquete("Quantas horas por semana voce usa redes sociais?",
-                          "Quantas horas por semana voce passa jogando?",
-                          "Quantas horas por semana voce passa estudando?",
-                          "2020-08-26")
-
-    if enquete_id == 3:
-        enquete = Enquete("Qual linguagem de programacao voce esta estudando?",
-                          "Quando voce iniciou os estudos nessa linguagem?",
-                          "Por qual meio voce esta estudando? (livro, youtube, cursos, etc)",
-                          "2020-08-27")
-    return render(request, 'enquete.html', {"enquete": enquete})
+    return render(request, 'enquete.html', enquete)
